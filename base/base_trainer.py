60d0db342c1bbc16e50687a07465b185c289b272
Vũ Hoàng Long <longvh.work@gmail.com>
2024-11-07 16:30:19 +0700

    fix: add missing newline at end of file in BaseTrainer class docstring

--- base/base_trainer.py
+++ base/base_trainer.py
@@ -15,14 +15,14 @@ class BaseTrainer:
         optimizer: torch.optim.Optimizer,
         config: dict, 
     ):
-    """
-    Initializes the base trainer with the given model, criterion, metric functions, optimizer and configuration.
-    Args:
-        model (torch.nn.Module): The model to be trained.
-        criterion (torch.nn.Module): The loss function.
-        metric_ftns (list): List of metric functions.
-        optimizer (torch.optim.Optimizer): The optimizer to be used.
-        config (dict): Configuration dictionary.
-    """
-    self.config = config
-    self.logger = config.get_logger('trainer', config['trainer']['verbosity'])
+        """
+        Initializes the base trainer with the given model, criterion, metric functions, optimizer and configuration.
+        Args:
+            model (torch.nn.Module): The model to be trained.
+            criterion (torch.nn.Module): The loss function.
+            metric_ftns (list): List of metric functions.
+            optimizer (torch.optim.Optimizer): The optimizer to be used.
+            config (dict): Configuration dictionary.
+        """
+        self.config = config
+        self.logger = config.get_logger('trainer', config['trainer']['verbosity'])
