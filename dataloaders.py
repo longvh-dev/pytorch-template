1cecf0107a3aaf6199a1e710761b0aa68d7874fa
Vũ Hoàng Long <longvh.work@gmail.com>
2024-11-20 09:46:08 +0700

    chore: comment out unused ImageNet dataset loading

--- dataloaders.py
+++ dataloaders.py
@@ -41,7 +41,7 @@ def get_dataloader(
         test_dataset = torchvision.datasets.CIFAR100(
             root='./data', train=False, download=True, transform=transform_test
         )
-    elif data_name == 'imagenet':
+    # elif data_name == 'imagenet':
         # train_dataset
     
     train_loader = BaseDataLoader(
